d3a54f8c43fcd06e8b0cb76c4f531b16782d0c66
BuildTools <unconfigured@null.spigotmc.org>
2023-06-19 16:32:52 -0400

    Key validation and new license

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BlockBreakerClient
-Client for BlockBreaker.
+
+This software was designed for students to aggravate their IT dudes. They started the conflict. I will end it.
+
+Client for the BlockBreaker project. TechnoDot is awesome.
---
+++ src/validate.py
@@ -0,0 +1,22 @@
+# Reverse engineering this file is against the TechnoLicense included with this project.
+# If you did not receive a copy of the license, please see https://github.com/BlockBreakerOfficial/BlockBreakerClient/blob/main/LICENSE
+
+a=int
+k=range
+f=str
+z=5
+i=[[10,21,32,43,20,31,42],[21,32,43,44,20,31,42],[31,32,33,34,42,43,44],[23,32,41,40,24,33,42],[23,32,41,40,24,33,42]]
+x=[[z,1,0,2,0,6,4,z,3],[0,1,3,z,3,6,z,1,2],[4,0,2,6,2,3,6,4,1],[0,1,2,4,2,3,4,0,6]]
+m=[80,89,84,65]
+p="https://techdude.pyt01234567e8e.com/api/validate"
+def validate(e):
+    global a,k,f
+    e=[e.split("-")[1][i:i+z] for i in k(z-z,len(e.split("-")[a(z/z)]),z)]
+    t=[]
+    g=ord(e[2][a(z/z)])
+    o=p
+    for c in k(a(z-z),z):
+        if ord(e[z-z][c])==84:
+            for j in i[c]:t.append(e[a(f(j)[a(z/z)])][a(f(j)[z-z])])
+    for s in x[m.index(g)]:o=o.replace(f(x[m.index(g)].index(s)),t[s].lower())
+    return o
